7b627fe4a40347e67ad801238374dbf2b22a447f
silasonyango93 <silas.onyango93@gmail.com>
2025-04-27 20:13:22 +0300

    residual plots deployment

--- Prediction_Residual_Plots.py
+++ Prediction_Residual_Plots.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from scipy.stats import norm
-import io
 import requests
 import os
 
